0507f607061a8cffa5ddff05f60c7e5a1c047420
shawnhr <shawnk723@live.com>
2017-09-21 22:56:22 -0700

    Completed getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,37 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+//You should use document.body, element.childNodes, and element.classList
+
+
+
+var getElementsByClassName = function(className){
+	// your code here
+
+	var childNode = function(el){
+	  var array = [];
+	  if(el.classList.contains(className)){
+	  	array = [el];
+	  }
+	  else{array = [];}
+	  
+	  return _.reduce(el.children, function(array, child){ return array.concat(childNode(child));},array);
+	};
+	return childNode(document.body);
 };
+
+
+// $('body').addClass('targetClassName');
+// htmlStrings.forEach(function(htmlString) {
+//   var $rootElement = $(htmlString);
+//   $('body').append($rootElement);
+
+//   var result = getElementsByClassName('targetClassName');
+//   var expectedNodeList = document.getElementsByClassName('targetClassName');
+//   var expectedArray = Array.prototype.slice.apply(expectedNodeList);
+//   var equality = _.isEqual(result, expectedArray); // why can't we use `===` here?
+//   expect(equality).to.equal(true);
+
+//   $rootElement.remove();
+// });
+// $('body').removeClass('targetClassName');
