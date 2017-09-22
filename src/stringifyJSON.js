79a44c541507ac3b807da030685a641921d2d9ba
shawnhr <shawnk723@live.com>
2017-09-22 01:22:28 -0700

    Completed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,34 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if( typeof obj === 'boolean'|| typeof obj === 'number')
+  	return obj.toString();
+  if( typeof obj === 'string')
+  	return '"'+obj+'"';
+  if( obj === undefined || obj === 'function')
+  	return undefined;
+  if( obj === null)
+  	return 'null';
+  if(typeof obj === 'object')
+
+  if(Array.isArray(obj)){
+  	var arr = [];
+  	obj.forEach(function(el){
+  	return arr.push(stringifyJSON(el));
+  	}); 
+  	return '[' + arr.join(',') + ']'; 	
+  }
+
+  if(typeof obj === 'object') {
+  	var res = [];
+  	for (var el in obj){
+  		if( typeof obj[el] !== 'function' && obj[el] !== undefined  )
+  			res.push(stringifyJSON(el) + ':' + stringifyJSON(obj[el])); 
+  	} 
+	return '{' + res.join(',') + '}';  
+   }
 };
+
+
+
+
